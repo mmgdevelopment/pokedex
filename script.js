a2055d615ca57e9c1591fa25ad430d440beb412c
mmgdevelopment <marcel_gregor@icloud.com>
2022-09-03 11:38:28 +0200

    regex id

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-const URL = 'https://pokeapi.co/api/v2/';
+const URL = 'https://pokeapi.co/api/v2/pokemon-species/';
 let pokemons = [];
-let resultAsJson;
+let rawPokemons = [];
 
 fetchData();
 
 async function fetchData(){
-    let result = await fetch(URL)
+
+    for (let i = 1; i < 15; i++) {
+        let result = await fetch(URL + i);
+        resultAsJson = await result.json();
+        rawPokemons.push(resultAsJson);
+    }
+
+};
+
+function init(){
+    renderCard();
+}
+
+function saveDatatLocal(){
+    for (let i = 0; i < resultAsJson.results.length; i++) {
+        const result = resultAsJson.results[i];
+        const name = result.name;
+        const id = result.url.split('pokemon/').pop().replace('/','');
+
+        pokemons.push(
+            {
+                id: id,
+                name: name,
+                type: 'red'
+            }
+        )
+    }
+    renderCard();
+}
+function renderCard(){
+    let canvas = document.getElementById('canvas');
+    canvas.innerHTML = '';
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        let id = pokemon.id;
+        let name = pokemon.name;
+        let type = pokemon.type;
+        canvas.innerHTML += htmlCard(id,name,type);        
+    }
 }
--- templates.js
+++ templates.js
@@ -1,1 +1,8 @@
-function htmlCard(id, mame, type, img)
+function htmlCard(id, name, type){
+return /*html*/`
+    <div id="${id}" class="${type}">
+            <h2>${name}</h2>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" alt="">          
+    </div>
+    `;
+}
