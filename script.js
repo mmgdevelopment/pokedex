b92e78302fed55b164faa44238fc7576812bcfcb
mmgdevelopment <marcel_gregor@icloud.com>
2022-09-03 10:15:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const URL = 'https://pokeapi.co/api/v2/';
+let pokemons = [];
+let resultAsJson;
+
+fetchData();
+
+async function fetchData(){
+    let result = await fetch(URL)
+}
---
+++ templates.js
@@ -0,0 +1,1 @@
+function htmlCard(id, mame, type, img)
